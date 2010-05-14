c6e739d40cc6e418118bdd02b4327199c1b94a9a
romulocmendes1 <romulocmendes1@af4b1a78-aede-02f8-0f0f-4074c07bec29>
2010-05-14 16:46:18 +0000

    Foi feito o código para configuração de todas as portas e clock do MSP430.

--- trunk/implementação/fw/controle/main.c
+++ trunk/implementação/fw/controle/main.c
@@ -8,19 +8,39 @@
 //#############################################################################
 //#############################################################################
 
+#define PIN_LED1    BIT4    // Porta 2
+#define PIN_LED2    BIT2    // Porta 2
+#define PIN_LED3    BIT3    // Porta 2
+
+#define PIN_INT1    BIT0    // Porta 2
+#define PIN_INT2    BIT1    // Porta 2
+
+#define PIN_MOSI    BIT1    // Porta 3
+#define PIN_MISO    BIT2    // Porta 3
+#define PIN_SCK     BIT3    // Porta 3
+#define PIN_CS      BIT4    // Porta 4
+
+//#############################################################################
+//#############################################################################
+
 int main( void )
 {
-  WDTCTL = WDTPW | WDTHOLD;             // Interrompe o temporizador watchdog.
+  WDTCTL = WDTPW | WDTHOLD;                  // Interrompe o watchdog timer.
   
-  BCSCTL1 = CALBC1_16MHZ;               // Configura o DCO para 16MHz.
+  BCSCTL1 = CALBC1_16MHZ;                    // Configura o DCO para 16MHz.
   DCOCTL  = CALDCO_16MHZ;
   
-  BCSCTL2 = BCSCTL2 | DIVS_3;           // Configura SMCLK para 2MHz.
+  BCSCTL2 = BCSCTL2 | DIVS_3;                // Configura SMCLK para 2MHz.
 
-  P4DIR = P4DIR & (~BIT3) & (~BIT5);   // Configura os pinos de interrupções.
-  P4IE  = P4IE & BIT3 & BIT5;
-  P4IES = 0;
-  P4IFG = P4IFG & (~BIT3) & (~BIT5);
+  P2DIR = P2DIR & (~PIN_INT1) & (~PIN_INT2); // Configura pinos INT1 e INT2.
+  P2IE  = P2IE & PIN_INT1 & PIN_INT2;
+  P2IES = 0;
+  P2IFG = P2IFG & (~PIN_INT1) & (~PIN_INT2);
+
+  P4DIR = P4DIR | PIN_CS;                         // Configura pino CS.
+  P4OUT = P4DIR | PIN_CS;
+  
+  P3SEL = P3SEL | PIN_MOSI | PIN_MISO | PIN_SCK;  // Configura pinos SPI.
 
 }
 
@@ -28,7 +48,7 @@ int main( void )
 //#############################################################################
 
 #pragma vector = PORT_INT_VECTOR
-__interrupt void Port_INT_ISR(void)
+__interrupt void interruption_vector(void)
 {
 
 }
