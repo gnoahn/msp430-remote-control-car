d8b55dabc06349ce232a89884dd7bc7bc63a0423
romulocmendes1 <romulocmendes1@af4b1a78-aede-02f8-0f0f-4074c07bec29>
2010-06-27 11:43:23 +0000

    Criado o projeto. Nesse 1o commit, o projeto é semelhante ao rf_2.

---
+++ implementação/fw/carrro/main_rf.c
@@ -0,0 +1,58 @@
+#include "include.h"
+
+char txBuffer[4];
+char rxBuffer[4];
+
+void main (void)
+{
+  WDTCTL = WDTPW | WDTHOLD;
+
+  SPIInitialization();
+  RFInitialization();
+  RFConfiguration();
+
+  // Configure Switch
+  P1REN = PIN_SWITCH;
+  P1IES = PIN_SWITCH;
+  P1IFG = P1IFG & (~PIN_SWITCH);
+  P1IE  = PIN_SWITCH;
+  
+  // Configure LEDs
+  P1OUT = P1OUT & (~PIN_LED1) & (~PIN_LED2);
+  P1DIR = P1DIR | PIN_LED1 | PIN_LED2;
+
+  // Configure GDO0 to RX packet info from CC2500
+  P2IES = P2IES | PIN_GDO0;
+  P2IFG = P2IFG & (~PIN_GDO0);
+  P2IE  = P2IE | PIN_GDO0;
+
+  WriteStrobe(SRX);
+
+  __bis_SR_register(LPM3_bits + GIE);
+}
+
+#pragma vector = PORT1_VECTOR // Interrupt from a pressed button
+__interrupt void port1_ISR (void)
+{
+  txBuffer[0] = 2;
+  txBuffer[1] = 0x01;
+  txBuffer[2] = (~P1IFG << 1) & 0x02;
+  
+  BurstWriteRegister(TXFIFO, txBuffer, 3);
+  WriteStrobe(STX);
+  
+  __delay_cycles(5000);
+  
+  P1IFG = P1IFG & (~PIN_SWITCH);
+}
+
+#pragma vector = PORT2_VECTOR // Interrupt from GDO0 (packet received)
+__interrupt void port2_ISR(void)
+{
+  char len = 2;
+  if (RFReceivePacket(rxBuffer,&len))
+  {
+    P1OUT = P1OUT ^ rxBuffer[1];
+  }
+  P2IFG = P2IFG & (~PIN_GDO0);
+}
